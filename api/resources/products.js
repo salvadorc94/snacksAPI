e7d99c7c4a254e12d42f9b9ee9004b28f6dd87da
salvadorc94 <secg.1994@gmail.com>
2018-12-05 20:33:49 -0600

    Added a middleware for handling resources and added the resources wihtouth functionality

---
+++ api/resources/products.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Obtain all the products'
+    });
+});
+
+router.post('/',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Add new product'
+    });
+});
+
+router.get('/sort/:sort',(req,res,next)=>{
+    //req.params.sort;
+    res.status(200).json({
+        message: 'Obtain all the products sorted'
+    });
+});
+
+router.get('/:productName',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Obtain a product by its name'
+    });
+});
+
+router.delete('/:productId/delete',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Delete a product'
+    });
+});
+
+router.patch('/:productId/priceUpdate',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Update price'
+    });
+});
+
+router.post('/:productId/buy',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Buy a product and modifie the stock'
+    });
+});
+
+router.post('/:productId/like',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Like a product and modify the likes'
+    });
+});
+router.post('/:productId/delLike',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Delete the like of a product'
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
-//ALL THE ENDPOINTS
-app.get("/", (req,res) => {
-    res.status(200).json({
-        message: 'Server up and running'
-    });
-});
+//MIDDLEWARE
+const productRoutes = require('./api/resources/products');
+const loginRoutes = require('./api/resources/login');
+app.use('/products',productRoutes);
+app.use('/login',loginRoutes);
 
+/*You could simply add the listen here to the app 
+but making it modular helps scalate quickly*/
 module.exports = app;
